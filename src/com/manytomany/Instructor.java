aeb792ffb4f2d7b2b7d893c7c8471013bdd57d3a
SathvikaTalakanti <sathvika.talakanti@zemosolabs.com>
2022-03-23 10:33:11 +0530

    initial commit

--- src/com/manytomany/Instructor.java
+++ src/com/manytomany/Instructor.java
@@ -25,9 +25,6 @@ public class Instructor {
     @JoinColumn(name = "instructor_detail_id")
     private InstructorDetail instructorDetail;
 
-    // delete cascade yok
-    // cunku instructor silindiginde kursun silinmesini istemiyoruz
-    //ayni sey kurs silindiginde de gecerli
     @OneToMany(fetch = FetchType.LAZY, mappedBy = "instructor" , cascade = {CascadeType.DETACH, CascadeType.MERGE,
             CascadeType.PERSIST, CascadeType.REFRESH})
     private List<Course> courses;
